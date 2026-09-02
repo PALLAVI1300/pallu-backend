7fe22cc394987abf8a2fb15b37338131128b5684
Pallavi <pallavi@gmail.com>
2026-09-02 20:36:20 +0530

    Setup dotenv and environment variables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
- console.log("Server is running...");
+  import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from Chai Aur Backend!");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
